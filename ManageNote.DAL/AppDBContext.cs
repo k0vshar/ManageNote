70edc745bab5c1a77b11504ff6a952e13daef543
k0vshar <www.ivankovshar.2004@gmail.com>
2024-03-26 18:11:57 +0200

    Add project files.

---
+++ ManageNote.DAL/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageNote.DAL
+{
+    internal class AppDBContext
+    {
+    }
+}
